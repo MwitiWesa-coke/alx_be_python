d0d4382ed67214af02fd30819bcb59be4dcc6d78
MwitiWesa-coke <wesamwiti01@gmail.com>
2025-06-29 21:08:40 +0300

    Creating a book instance and utilizing the implemented magic methods

---
+++ book_class.py
@@ -0,0 +1,14 @@
+class book:
+    def __init__(self, title, author, year)
+    selt.title = title
+    self.author = author
+    self.year = year
+
+    def __del__(self)
+    print("Deleting {self.title}")
+
+    def __str__(self)
+    return "{self.title} by {self.author}, published in {self.year}"
+
+    def __repr__(self)
+    return f"Book('{self.title}', '{self.author}', {self.year})"
