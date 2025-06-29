4823d5213faecc61f173c1f8635a3c8a1fa561bc
MwitiWesa-coke <wesamwiti01@gmail.com>
2025-06-29 23:28:28 +0300

    unnittest feels nice

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -7,7 +7,7 @@ class TestSimpleCalculator(unittest.TestCase):
         """Create a calculator object before each test."""
         self.calc = SimpleCalculator()
 
-    def test_add(self):
+    def test_addition(self):
         self.assertEqual(self.calc.add(2, 3), 5)
         self.assertEqual(self.calc.add(-2, 3), 1)
         self.assertEqual(self.calc.add(0, 0), 0)
